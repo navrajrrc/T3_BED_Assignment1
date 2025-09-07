16fcbb99d0ec1289aad07ad3f2f00caefb805c1d
Navraj Singh <navrajsingh@rrc.ca>
2025-09-07 00:09:04 -0500

    Done tests for the health check endpoints

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,26 @@ import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
+interface HealthCheckResponse {
+    status:string;
+    uptime: number;
+    timestamp:string;
+    version: string;
+}
+
 app.get("/", (req: Request, res: Response) => {
     res.send("Hello World");
 });
+
+app.get("/api/v1/health", (req:Request, res: Response) => {
+    const healthData: HealthCheckResponse = {
+        status: "OK",
+        uptime:process.uptime(),
+        timestamp: new Date().toISOString(),
+        version: "1.0.0",
+    };
+
+    res.json(healthData);
+})
 
 export default app;
--- test/app.test.ts
+++ test/app.test.ts
@@ -9,4 +9,15 @@ describe("GET /", () => {
         expect(response.text).toBe("Hello World");
     
     });
+});
+
+describe("GET /api/v1/health", () => {
+    it("should return health status", async () => {
+        const response: Response = await request(app).get("/api/v1/health");
+
+        expect(response.body.status).toBe("OK");
+        expect(response.body).toHaveProperty("uptime");
+        expect(response.body).toHaveProperty("timestamp");
+        expect(response.body).toHaveProperty("version", "1.0.0");
+    });
 })
